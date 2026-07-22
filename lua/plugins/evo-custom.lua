a060ddfce1c3a8144d7989653fac4d69f6fed7c2
Evo Kellerman <evo@bowerhousedigital.com.au>
2026-07-22 10:22:09 +1000

    NEW: Opencode.nvim

--- lua/plugins/evo-custom.lua
+++ lua/plugins/evo-custom.lua
@@ -3,4 +3,93 @@ return {
     "akinsho/bufferline.nvim",
     enabled = false,
   },
+  {
+    "nickjvandyke/opencode.nvim",
+    version = "*", -- Latest stable release
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any; goto definition on the type for details
+      }
+
+      vim.o.autoread = true -- Required for `vim.g.opencode_opts.events.reload`
+
+      -- Recommended/example keymaps
+      vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+        require("opencode").ask("@this: ")
+      end, { desc = "Ask OpenCode…" })
+      vim.keymap.set({ "n", "x" }, "<leader>os", function()
+        require("opencode").select()
+      end, { desc = "Select OpenCode…" })
+
+      vim.keymap.set({ "n", "x" }, "go", function()
+        return require("opencode").operator("@this ")
+      end, { desc = "Append range to OpenCode", expr = true })
+      vim.keymap.set("n", "goo", function()
+        return require("opencode").operator("@this ") .. "_"
+      end, { desc = "Append line to OpenCode", expr = true })
+
+      vim.keymap.set("n", "<S-C-u>", function()
+        require("opencode").command("session.half.page.up")
+      end, { desc = "Scroll OpenCode up" })
+      vim.keymap.set("n", "<S-C-d>", function()
+        require("opencode").command("session.half.page.down")
+      end, { desc = "Scroll OpenCode down" })
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        win = {
+          input = {
+            keys = {
+              -- Disable the default <C-h> mapping so it falls back to your global tmux- navigator mapping
+              ["<C-h>"] = false,
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "3rd/diagram.nvim",
+    dependencies = {
+      { "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
+    },
+    opts = { -- you can just pass {}, defaults below
+      events = {
+        render_buffer = { "InsertLeave", "BufWinEnter", "TextChanged" },
+        clear_buffer = { "BufLeave" },
+      },
+      renderer_options = {
+        mermaid = {
+          background = nil, -- nil | "transparent" | "white" | "#hex"
+          theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+          width = nil, -- nil | 800 | 400 | ...
+          height = nil, -- nil | 600 | 300 | ...
+          cli_args = nil, -- nil | { "--no-sandbox" } | { "-p", "/path/to/puppeteer" } | ...
+        },
+        plantuml = {
+          charset = nil,
+          cli_args = nil, -- nil | { "-Djava.awt.headless=true" } | ...
+        },
+        d2 = {
+          theme_id = nil,
+          dark_theme_id = nil,
+          scale = nil,
+          layout = nil,
+          sketch = nil,
+          cli_args = nil, -- nil | { "--pad", "0" } | ...
+        },
+        gnuplot = {
+          size = nil, -- nil | "800,600" | ...
+          font = nil, -- nil | "Arial,12" | ...
+          theme = nil, -- nil | "light" | "dark" | custom theme string
+          cli_args = nil, -- nil | { "-p" } | { "-c", "config.plt" } | ...
+        },
+      },
+    },
+  },
 }
